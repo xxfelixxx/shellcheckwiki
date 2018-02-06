51e9536526576a9e7d971a35fcb2607ffce09c44
Felix Tubiana <felixtubiana@gmail.com>
2018-02-06 01:16:13 +0000

    Version 0.03

--- lib/App/ShellCheckWiki.pm
+++ lib/App/ShellCheckWiki.pm
@@ -7,7 +7,7 @@ use warnings;
 use Term::ANSIColor;
 use WWW::Mechanize;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 my $formats = {
     '# '   => 'bold green',   # H1
@@ -146,7 +146,7 @@ App::ShellCheckWiki - Check the wiki for details about shellcheck errors.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
